c4454f5f3f9da13bb7976bb1b9af2c48197cd425
Jordan <jrdnbrkfld@icloud.com>
2022-12-03 19:26:04 +0000

    created basic NavBar will be changed

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const NavBar = () => {
+  return (
+    <Navbar expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>DevLocker</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link href="#home"><i class="fa-solid fa-house"></i>Home</Nav.Link>
+            <Nav.Link><i class="fa-solid fa-right-to-bracket"></i>Sign in</Nav.Link>
+            <Nav.Link><i class="fa-solid fa-pen-nib"></i>Sign up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
