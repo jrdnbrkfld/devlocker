6a4cd5640e49587a884310ee195978d194218b29
Jordan <jrdnbrkfld@icloud.com>
2022-12-03 19:25:48 +0000

    imported NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import { Button } from 'react-bootstrap';
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>DevLocker</h1>
-      <Button variant="primary">Primary</Button>
+      <NavBar />
     </div>
   );
 }
