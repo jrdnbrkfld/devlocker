4e4c30688d190caaf53bbdaea9abb18dca9881e0
Jordan <jrdnbrkfld@icloud.com>
2022-12-04 16:44:23 +0000

    added react-router-dom to handle links to different pages of the website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import { Container } from 'react-bootstrap';
+import { Route, Switch } from 'react-router-dom'
 
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route render={() => <h1>Page not found!</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
