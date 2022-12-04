4c678688a8f857b6a4d37c66887c8f788699c922
Jordan <jrdnbrkfld@icloud.com>
2022-12-04 13:57:58 +0000

    made changes to CSS for NavBar and App

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import styles from '../styles/NavBar.module.css'
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar className={styles.NavBar}expand="md" fixed="top" variant="dark">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>DevLocker</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="#home">
+          <span>DevLocker</span>
+        </Navbar.Brand>
+        <Navbar.Toggle
+          class="custom-toggler"
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link href="#home"><i class="fa-solid fa-house"></i>Home</Nav.Link>
-            <Nav.Link><i class="fa-solid fa-right-to-bracket"></i>Sign in</Nav.Link>
-            <Nav.Link><i class="fa-solid fa-pen-nib"></i>Sign up</Nav.Link>
+            <Nav.Link>
+              <i class="fa-solid fa-right-to-bracket"></i>
+              <span>Sign In</span>
+            </Nav.Link>
+            <Nav.Link>
+              <i class="fa-solid fa-user-plus"></i>
+              <span>Sign Up</span>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
