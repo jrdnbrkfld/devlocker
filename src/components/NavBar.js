6b30ed5a90ac065cab7f74838b2e0d16058f9328
Jordan <jrdnbrkfld@icloud.com>
2022-12-04 16:45:06 +0000

    link urls to the different pages of the website

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,25 +2,38 @@ import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import logo from "../assets/logo.png";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top" variant="dark">
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="90" />
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="90" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
               <i class="fa-solid fa-right-to-bracket"></i>
               <span>Sign In</span>
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signup"
+            >
               <i class="fa-solid fa-user-plus"></i>
               <span>Sign Up</span>
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
