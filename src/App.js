01385dace212c473fcc27d58f76755b30b6c22e1
Jordan <jrdnbrkfld@icloud.com>
2022-12-05 22:57:26 +0000

    created SignIn.js with a working form shows errors when invalid data is used

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import SignUp from './pages/auth/SignUp';
+import SignIn from './pages/auth/SignIn';
 import { Container } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom'
 import './api/axiosDefaults'
@@ -13,7 +14,7 @@ function App() {
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Home</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route exact path="/signin" render={() => <SignIn />} />
           <Route exact path="/signup" render={() => <SignUp />} />
           <Route render={() => <h1>Page not found!</h1>} />
         </Switch>
---
+++ src/pages/auth/SignIn.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { Alert, Button, Col, Container, Form } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import styles from "../../styles/SignUp.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Buttons.module.css";
+import axios from "axios";
+
+function SignIn() {
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = signInData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/login/", signInData);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+      console.log(err);
+    }
+  };
+
+  return (
+    <Col className="my-auto p-0 p-md-2">
+      <Container className={`${appStyles.Content} p-4 `}>
+        <h1 className={styles.Header}>sign in</h1>
+        <Form className="w-50" onSubmit={handleSubmit}>
+          <Form.Group controlId="username">
+            <Form.Label className="d-none">Username</Form.Label>
+            <Form.Control
+              value={username}
+              onChange={handleChange}
+              className={styles.Input}
+              type="text"
+              name="username"
+              placeholder="Enter your username"
+            />
+          </Form.Group>
+          {errors.username?.map((message, idx) => (
+            <Alert key={idx} variant="danger">
+              {message}
+            </Alert>
+          ))}
+
+          <Form.Group controlId="password">
+            <Form.Label className="d-none">Password</Form.Label>
+            <Form.Control
+              value={password}
+              onChange={handleChange}
+              className={styles.Input}
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+            />
+          </Form.Group>
+          {errors.password?.map((message, idx) => (
+            <Alert key={idx} variant="danger">
+              {message}
+            </Alert>
+          ))}
+
+          <Button
+            className={`${btnStyles.Button} ${btnStyles.Wide}`}
+            type="submit"
+          >
+            Sign in
+          </Button>
+          {errors.non_field_errors?.map((message, idx) => (
+            <Alert key={idx} variant="danger" className="mt-3">
+              {message}
+            </Alert>
+          ))}
+
+        </Form>
+      </Container>
+      <Container className="mt-3">
+        <Link className={styles.Link} to="/signup">
+          Don't have an account yet? Click here to sign up
+        </Link>
+      </Container>
+    </Col>
+  );
+}
+
+export default SignIn;
