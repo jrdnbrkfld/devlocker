27f83374d99ec11f39825e9009c3a2586c89773c
Jordan <jrdnbrkfld@icloud.com>
2022-12-05 20:37:54 +0000

    fixed baseURL accidentally linked to the react app, not the api

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://devlocker.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = "https://devlocker-api.herokuapp.com";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
