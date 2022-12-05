9d639b142fae7fd267f6632ee590a5818fdd6cf0
Jordan <jrdnbrkfld@icloud.com>
2022-12-05 13:20:01 +0000

    linked route to SignIn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import SignUp from './pages/auth/SignUp';
 import { Container } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom'
 import './api/axiosDefaults'
@@ -13,7 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
-          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route exact path="/signup" render={() => <SignUp />} />
           <Route render={() => <h1>Page not found!</h1>} />
         </Switch>
       </Container>
