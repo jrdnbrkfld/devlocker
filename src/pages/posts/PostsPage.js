a3c116d0ebc099ffad17879f340f2bf090662247
Jordan <jrdnbrkfld@icloud.com>
2023-01-05 09:59:00 +0000

    added infinite scroll to home page

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -15,6 +15,8 @@ import Post from "./Post";
 
 import NoResults from "../../assets/no-results.png";
 import Asset from "../../components/Asset";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -63,9 +65,15 @@ function PostsPage({ message, filter = "" }) {
         {hasLoaded ? (
           <>
             {posts.results.length ? (
-              posts.results.map((post) => (
-                <Post ket={post.id} {...post} setPosts={setPosts} />
-              ))
+              <InfiniteScroll
+                children={posts.results.map((post) => (
+                  <Post ket={post.id} {...post} setPosts={setPosts} />
+                ))}
+                dataLength={posts.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => fetchMoreData(posts, setPosts)}
+              />
             ) : (
               <Container className={styles.Container}>
                 <Asset src={NoResults} message={message} />
