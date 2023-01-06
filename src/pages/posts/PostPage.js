0af686b3805e12c0ff3bcbe6a5bfc3541fe327e9
Jordan <jrdnbrkfld@icloud.com>
2023-01-06 14:14:33 +0000

    added infinite scroll to comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -14,6 +14,10 @@ import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Comment from "../comments/Comment";
 
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
+
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
@@ -57,14 +61,20 @@ function PostPage() {
           ) : null}
           <Container className={`${appStyles.Content} ${styles.Form}`}>
             {comments.results.length ? (
-              comments.results.map((comment) => (
-                <Comment
-                  key={comment.id}
-                  {...comment}
-                  setPost={setPost}
-                  setComments={setComments}
-                />
-              ))
+              <InfiniteScroll
+                children={comments.results.map((comment) => (
+                  <Comment
+                    key={comment.id}
+                    {...comment}
+                    setPost={setPost}
+                    setComments={setComments}
+                  />
+                ))}
+                dataLength={comments.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!comments.next}
+                next={() => fetchMoreData(comments, setComments)}
+              />
             ) : currentUser ? (
               <span>No comments yet, be the first!</span>
             ) : (
