99220ffe7c2f95ebf5ee7e23e868c0343dc2e4ce
Jordan <jrdnbrkfld@icloud.com>
2023-01-06 10:26:33 +0000

    added comments section to posts

---
+++ src/pages/comments/CommentCreateForm.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+import styles from "../../styles/CommentCreateEditForm.module.css";
+import btnStyles from "../../styles/Buttons.module.css";
+import ProfilePhoto from "../../components/ProfilePhoto";
+import { axiosRes } from "../../api/axiosDefaults";
+
+function CommentCreateForm(props) {
+  const { post, setPost, setComments, profileImage, profile_id } = props;
+  const [content, setContent] = useState("");
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axiosRes.post("/comments/", {
+        content,
+        post,
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: [data, ...prevComments.results],
+      }));
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count + 1,
+          },
+        ],
+      }));
+      setContent("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form
+      className={`mt-2 
+    ${styles.Form}`}
+      onSubmit={handleSubmit}
+    >
+      <Form.Group>
+        <InputGroup>
+          <Link to={`/profiles/${profile_id}`}>
+            <ProfilePhoto src={profileImage} />
+          </Link>
+          <Form.Control
+            className={styles.Comment}
+            placeholder="Say something"
+            as="textarea"
+            value={content}
+            onChange={handleChange}
+            rows={2}
+          />
+        </InputGroup>
+      </Form.Group>
+      <button
+        className={`${btnStyles.Button} btn d-block ml-auto`}
+        disabled={!content.trim()}
+        type="submit"
+      >
+        post
+      </button>
+    </Form>
+  );
+}
+
+export default CommentCreateForm;
