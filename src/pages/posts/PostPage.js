2c80083f4ef68ae245268dd17a8e848914aa8ecd
Jordan <jrdnbrkfld@icloud.com>
2023-01-06 10:59:41 +0000

    added api request and comments section

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -4,6 +4,7 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
+import styles from "../../styles/CommentCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
@@ -11,6 +12,7 @@ import Post from "./Post";
 
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Comment from "../comments/Comment";
 
 function PostPage() {
   const { id } = useParams();
@@ -23,11 +25,12 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?posts=${id}`),
         ]);
         setPost({ results: [post] });
-        console.log(post);
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -52,6 +55,17 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
+          <Container className={`${appStyles.Content} ${styles.Form}`}>
+            {comments.results.length ? (
+              comments.results.map((comment) => (
+                <Comment key={comment.id} {...comment} />
+              ))
+            ) : currentUser ? (
+              <span>No comments yet, be the first!</span>
+            ) : (
+              <span>No comments yet</span>
+            )}
+          </Container>
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
