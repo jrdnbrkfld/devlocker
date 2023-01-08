0eb239cad742977a82ea52c861520ac9d314ad5b
Jordan <jrdnbrkfld@icloud.com>
2023-01-08 19:20:22 +0000

    changed format for <p> tag

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -14,7 +14,7 @@ const PopularProfiles = ({ mobile }) => {
     >
       {popularProfiles.results.length ? (
         <>
-          <p>Most Followed Profiles!</p>
+          <p>Most followed profiles</p>
           {mobile ? (
             <div className="d-flex justify-content-around">
               {popularProfiles.results.slice(0, 4).map((profile) => (
