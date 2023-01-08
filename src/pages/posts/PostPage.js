fcb0c1c2e5380e835582762c184261df9495f9fc
Jordan <jrdnbrkfld@icloud.com>
2023-01-08 19:20:45 +0000

    added margin and import PopularProfiles for mobile

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -44,9 +44,9 @@ function PostPage() {
   }, [id]);
 
   return (
-    <Row className="h-100">
+    <Row className="h-100 m-1">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
+        <PopularProfiles mobile />
         <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
           {currentUser ? (
