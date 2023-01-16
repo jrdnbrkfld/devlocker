c7ed3b9e5653b237a85297cc1d1b2c47dec52347
Jordan <jrdnbrkfld@icloud.com>
2023-01-16 00:27:03 +0000

    deleted unused link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -127,12 +127,6 @@ const NavBar = () => {
         />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto mt-1 text-left">
-            <NavLink
-              exact
-              className={`${styles.NavLink} ${styles.Toggler}`}
-              activeClassName={styles.Active}
-              to="/"
-            ></NavLink>
             {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
